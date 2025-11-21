d35b85a469c62c2265a80203cd4c8070325c363f
denizdogruel00 <denizyazilm@gmail.com>
2025-11-21 12:51:48 +0300

    detay sayfasına değişkenlerden veri çekildi

---
+++ dotnetbasics2/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetbasics2.Controllers
+{
+    public class ProductController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            string productName="İphone 13";
+            int productPrice=50000;
+            string productDescription="Geleceğin telefonu";
+            string productImage="İphone13.jpg";
+            bool satisDurumu=true;
+            ViewData["ProductName"]=productName;
+            ViewData["ProductPrice"]=productPrice;
+            ViewData["ProductDescription"]=productDescription;
+            ViewData["ProductImage"]=productImage;
+            ViewData["SatisDurumu"]=satisDurumu;
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
